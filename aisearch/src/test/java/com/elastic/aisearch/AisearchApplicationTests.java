87c0278dc1d60bb388778e263b30a17056cb5d21
Matheus <math.mds147@gmail.com>
2025-03-21 10:06:30 -0300

    feat/basic-rest-controller

---
+++ aisearch/src/test/java/com/elastic/aisearch/AisearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastic.aisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
