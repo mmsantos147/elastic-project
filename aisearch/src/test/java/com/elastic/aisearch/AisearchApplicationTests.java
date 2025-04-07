c1cdbce754b2182de3feec29c78722eb0d6ef644
Matheus <math.mds147@gmail.com>
2025-04-07 10:56:33 -0300

    Revert "reset"
    This reverts commit ab9c5047776c3259f14562501e53658620e7f330.

--- aisearch/src/test/java/com/elastic/aisearch/AisearchApplicationTests.java
+++ aisearch/src/test/java/com/elastic/aisearch/AisearchApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.aisearch;
+package com.elastic.aisearch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AisearchApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
