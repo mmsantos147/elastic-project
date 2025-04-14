a95073925974cd20d4ce16d07dce5ffca6b9eb71
joaoragazzo <joao_ragazzo@hotmail.com>
2025-04-14 11:27:10 -0300

    debbuging

--- frontend/src/api/search.api.js
+++ frontend/src/api/search.api.js
@@ -1,11 +1,11 @@
 import { makeRequest } from "./makeRequest"
 
 const search = async (content) => {
-    await makeRequest('get', `search?query=${content}`);
+    return await makeRequest('get', `search?query=${content}`);
 }
 
 const fetchHistory = async (content) => {
-    await makeRequest('post', 'history', content);
+    return await makeRequest('post', 'history', content);
 }
 
 export default { search, fetchHistory };
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const APP_NAME_CAMMEL_CASE = "uAISearch";
 export const APP_NAME_UPPERCASE = "uAISEARCH";
 
-export const ROOT_URL = "http://uaisearch.0x6a70.com/";
-export const API_PREFIX = "v1/";
+export const ROOT_URL = "http://uaisearch.0x6a70.com";
+export const API_PREFIX = "v1";
