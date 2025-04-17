5c39ee00418f583b7ceff78866ef0c73edc25e9f
joaoragazzo <joao_ragazzo@hotmail.com>
2025-04-17 20:41:57 -0300

    refact: center the debug controller

--- aisearch/src/main/java/com/elastic/aisearch/controller/AiSearchController.java
+++ aisearch/src/main/java/com/elastic/aisearch/controller/AiSearchController.java
@@ -1,23 +1,33 @@
 package com.elastic.aisearch.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.StringReader;
 
-@RestController
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.elastic.aisearch.parser.ParseException;
+import com.elastic.aisearch.parser.QueryParser;
+
+@Controller("/debug")
 public class AiSearchController {
     @GetMapping("/")
     public String hello() {
-        return "hello world!!!!!!!!!!!!!!!!!!!!";
+        return "Hello world!";
     }
 
-    @GetMapping("/test")
-    public String aaaa() {
-        return "ssssssssss";
-    }
+    @GetMapping("/queryDebug")
+    public ResponseEntity<String> parseQueryDebug(@RequestParam("q") String query) {
+        QueryParser queryParser = new QueryParser(new StringReader(query));
 
+        try {
+            String parsed = queryParser.parseQuery(query).toString();
 
-    @GetMapping("/cicd")
-    public String cicdTesting() {
-        return "CI/CD WORKING FINE!";
+            return new ResponseEntity<>(parsed, HttpStatus.OK);
+        } catch (ParseException e) {
+            return new ResponseEntity<>("Erro no parser: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
