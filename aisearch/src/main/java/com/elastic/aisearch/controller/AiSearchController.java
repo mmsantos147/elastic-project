e75bcd89d0e60f45fac39ed6084d22b11a5610c5
joaoragazzo <joao_ragazzo@hotmail.com>
2025-04-18 12:56:06 -0300

    fix: fix wrong request mapping

--- aisearch/src/main/java/com/elastic/aisearch/controller/AiSearchController.java
+++ aisearch/src/main/java/com/elastic/aisearch/controller/AiSearchController.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.elastic.aisearch.parser.ParseException;
 import com.elastic.aisearch.parser.QueryParser;
 
-@Controller("/debug")
+@Controller
+@RequestMapping("/debug")
 public class AiSearchController {
     @GetMapping("/")
     public String hello() {
--- aisearch/src/main/java/com/elastic/aisearch/controller/UserController.java
+++ aisearch/src/main/java/com/elastic/aisearch/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(path = "api/user")
+@RequestMapping(path = "user")
 public class UserController {
     private final UserService userService;
     private final UserSession userSession;
