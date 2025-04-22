e2a7b8c3f8a3d17fa430afb799f2d351eb36dcda
joaoragazzo <joao_ragazzo@hotmail.com>
2025-04-22 09:43:28 -0300

    fix: add more instructions to prevent json miss-formation

--- aisearch/src/main/java/com/elastic/aisearch/service/ChatGptService.java
+++ aisearch/src/main/java/com/elastic/aisearch/service/ChatGptService.java
@@ -23,9 +23,11 @@ public class ChatGptService {
                                 "Você é um agente feito para realizar resumos (em português) de resultados de uma pesquisa na Wikipédia." + 
                                 "Você deve gerar resumos pequenos (de no máximo 3 paragrafos) para os três primeiros resultados mais relevantes da pesquisa." +
                                 "Responda em formato JSON, com o campo 'title' (para o resumo), uma lista de 3 paragrafos, cada um tendo o campo 'content' e" + 
-                                "'url'. O campo 'content' você deve colocar o resumo do conteudo da 'url' que você pegou. Segue abaixo o resultado das três " + 
+                                "'url'. O campo 'content' você deve colocar o resumo do conteudo da 'url' que você pegou. Segue abaixo o resultado das três " +
                                 "primeiras pesquisas. A resposta deve estar em formato JSON puro, sem blocos de código, sem markdown e sem crases (`)." + 
-                                "Retorne apenas o JSON direto, sem explicações.."),
+                                "Retorne apenas o JSON direto, sem explicações.." + 
+                                "Coloque no formato {'title': 'TITULO', 'paragraphs': [{'content': 'RESUMO', 'url': 'URL'},{'content': 'RESUMO', 'url': 'URL" +
+                                "'},{'content': 'RESUMO', 'url': 'URL'}]}. Certifique-se de que a estrutura do JSON é válida."),
                         Map.of("role", "user", "content", searchResults)));
 
         return openAiWebClient.post()
