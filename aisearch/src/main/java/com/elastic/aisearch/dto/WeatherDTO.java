37a8cf543732a023e237bcb07e75ad03705087ad
Matheus <math.mds147@gmail.com>
2025-04-24 17:06:48 -0300

    fix: Weather Report

--- aisearch/src/main/java/com/elastic/aisearch/dto/WeatherDTO.java
+++ aisearch/src/main/java/com/elastic/aisearch/dto/WeatherDTO.java
@@ -2,6 +2,7 @@ package com.elastic.aisearch.dto;
 
 public record WeatherDTO(
         Integer id,
+        String group,
         String city,
         Integer temperature,
         String description
--- aisearch/src/main/java/com/elastic/aisearch/service/WeatherService.java
+++ aisearch/src/main/java/com/elastic/aisearch/service/WeatherService.java
@@ -29,6 +29,7 @@ public class WeatherService {
 
         return new WeatherDTO(
                 json.path("weather").path("id").asInt(),
+                json.path("weather").path("main").asText(),
                 json.path("name").asText(),
                 json.path("main").path("temp").asInt(),
                 json.path("weather").get(0).path("description").asText()
