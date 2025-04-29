ccfa4060eb64ffaa361387b29ca6dfba6df0d0b6
joaoragazzo <joao_ragazzo@hotmail.com>
2025-04-29 10:36:07 -0300

    refact: stauts code

--- aisearch/src/main/java/com/elastic/aisearch/controller/GlobalExceptionHandler.java
+++ aisearch/src/main/java/com/elastic/aisearch/controller/GlobalExceptionHandler.java
@@ -2,15 +2,15 @@ package com.elastic.aisearch.controller;
 
 import com.elastic.aisearch.dto.Messages.FailMessageDTO;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalStateException.class)
-    public FailMessageDTO exceptionHandler(Exception e) {
-        return new FailMessageDTO(
-                e.getMessage()
-        );
+    public ResponseEntity<FailMessageDTO> exceptionHandler(Exception e) {
+        return ResponseEntity.badRequest().body(new FailMessageDTO(
+                e.getMessage()));
     }
 }
