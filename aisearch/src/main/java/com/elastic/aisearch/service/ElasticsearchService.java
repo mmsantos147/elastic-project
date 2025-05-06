11404cf8d0596264133e03f8e2d6fde4b00eea99
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-06 19:19:54 -0300

    fix: parser AGAIN

--- aisearch/src/main/java/com/elastic/aisearch/service/ElasticsearchService.java
+++ aisearch/src/main/java/com/elastic/aisearch/service/ElasticsearchService.java
@@ -34,7 +34,7 @@ import java.util.Map;
 import java.util.Objects;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class ElasticsearchService {
 
@@ -45,6 +45,7 @@ public class ElasticsearchService {
     @Value("${elasticsearch.index.name:wikipedia}")
     private String indexName;
 
+    @Autowired
     private QueryParser queryParser;
 
     /**
