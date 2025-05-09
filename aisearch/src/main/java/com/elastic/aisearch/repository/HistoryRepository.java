78e3c32d762243bb28d675573e201b2904221188
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-08 21:53:21 -0300

    fix: fix the user id reference

--- aisearch/src/main/java/com/elastic/aisearch/repository/HistoryRepository.java
+++ aisearch/src/main/java/com/elastic/aisearch/repository/HistoryRepository.java
@@ -10,6 +10,6 @@ public interface HistoryRepository extends JpaRepository<History, Integer> {
     @Query("select s from History s where s.user.id = ?1 ORDER BY s.id desc")
     List<History> findMostRecentHistory(Integer userId);
 
-    @Query("delete from History s where s.user =?1")
+    @Query("delete from History s where s.user.id =?1")
     void deleteAllByUserId(Integer userId);
 }
