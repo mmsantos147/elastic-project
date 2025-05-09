f5ca91016803ea35c9dbf281684e09cceb347a87
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-09 15:33:07 -0300

    fix: add transactional and modify at history repository

--- aisearch/src/main/java/com/elastic/aisearch/repository/HistoryRepository.java
+++ aisearch/src/main/java/com/elastic/aisearch/repository/HistoryRepository.java
@@ -2,7 +2,9 @@ package com.elastic.aisearch.repository;
 
 import com.elastic.aisearch.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,6 +12,8 @@ public interface HistoryRepository extends JpaRepository<History, Integer> {
     @Query("select s from History s where s.user.id = ?1 ORDER BY s.id desc")
     List<History> findMostRecentHistory(Integer userId);
 
+    @Modifying
+    @Transactional
     @Query("delete from History s where s.user.id =?1")
     void deleteAllByUserId(Integer userId);
 }
