7cac3739283108546424fb90d64bf48a6f7365af
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-09 17:13:31 -0300

    feat: add mock server for developing frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],server: {
+    proxy: {
+      '/v1': 'http://localhost:3000/',
+    },
+  },
 })
---
+++ mock_server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Endpoint /v1/search/suggestions
+app.post("/v1/search/suggestions", (req, res) => {
+  const { query } = req.body;
+
+  if (query && query.trim().length > 0) {
+    const suggestions = [
+      "<i>De dicto</i> and <i>de re</i>",
+      "Gravity Probe A",
+      "Ankeny–Artin–Chowla congruence",
+      "List of commutative algebra topics",
+      "Artinian module",
+    ];
+    return res.json({ suggestions });
+  } else {
+    const suggestions = [];
+    return res.status(200).json({ suggestions });
+  }
+});
+
+app.get("/v1/history", (req, res) => {
+  const history = [
+    { id: 1, content: "history_element_1" },
+    { id: 2, content: "history_element_2" },
+    { id: 3, content: "history_element_3" },
+  ];
+
+  return res.status(200).json(history)
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
