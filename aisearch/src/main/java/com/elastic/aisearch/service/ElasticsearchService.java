8bc1eeacbdf931c4fe738877456220a27969455e
Matheus <math.mds147@gmail.com>
2025-05-12 10:56:52 -0300

    Update ElasticsearchService.java

--- aisearch/src/main/java/com/elastic/aisearch/service/ElasticsearchService.java
+++ aisearch/src/main/java/com/elastic/aisearch/service/ElasticsearchService.java
@@ -251,7 +251,7 @@ public class ElasticsearchService {
 
     private String treatContent(String content) {
         content = content.replaceAll("</?(som|math)\\d*>", "");
-        content = content.replaceAll("[^A-Za-z\\s<>]*(?!strong)[^A-Za-z\\s<>]+", "");
+        content = content.replaceAll("[^A-Za-z\\s\\d]+", "");
         content = content.replaceAll("\\s+", " ");
         content = content.replaceAll("^\\s+", "");
         return content;
