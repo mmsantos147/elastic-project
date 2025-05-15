da93d3a267f2465ed52a1ee4e9b820c0270a15ea
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-15 14:47:09 -0300

    feat: add graphnode constructor

--- aisearch/src/main/java/com/elastic/aisearch/controller/GraphController.java
+++ aisearch/src/main/java/com/elastic/aisearch/controller/GraphController.java
@@ -1,14 +1,18 @@
 package com.elastic.aisearch.controller;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elastic.aisearch.dto.graph.ArticleDTO;
+import com.elastic.aisearch.dto.graph.GraphNodeDTO;
 import com.elastic.aisearch.service.GraphService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,6 +28,12 @@ public class GraphController {
     public ResponseEntity<String> bulk(@RequestBody List<ArticleDTO> articleDTOs) {
         graphService.bulkArticles(articleDTOs); 
         graphService.generateConnections(articleDTOs);
-        return ResponseEntity.ok().body("OK");
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/{id}/{depth}")
+    public ResponseEntity<GraphNodeDTO> fetchGraph(@PathVariable("id") Integer id, @PathVariable("depth") Integer depth) {
+        GraphNodeDTO graphNode = graphService.generateGraph(id, 0, depth, Collections.emptySet());
+        return ResponseEntity.ok(graphNode);
     }
 }
