77994cf1c5a1cc7e52bcdd9621d7c82436c14c8f
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-15 18:04:03 -0300

    fix: correct method for graph api

--- frontend/src/api/Graph.api.js
+++ frontend/src/api/Graph.api.js
@@ -5,7 +5,7 @@ import { ApiContext } from '../context/ApiContext';
 export const useGraphService = () => {
   const { makeRequest } = useContext(ApiContext); 
 
-  const getGraph = (id, depth) => makeRequest('post', `/graph/${id}/${depth}`);
+  const getGraph = (id, depth) => makeRequest('get', `/graph/${id}/${depth}`);
 
   return { getGraph };
 };
