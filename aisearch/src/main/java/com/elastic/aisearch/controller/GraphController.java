da8211d3b5ff42f6be8058935624c7b76c187692
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-15 13:11:32 -0300

    fix: decorate func param with request body

--- aisearch/src/main/java/com/elastic/aisearch/controller/GraphController.java
+++ aisearch/src/main/java/com/elastic/aisearch/controller/GraphController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class GraphController {
     private final GraphService graphService;
 
     @PostMapping("/bulk")
-    public ResponseEntity<String> bulk(List<ArticleDTO> articleDTOs) {
+    public ResponseEntity<String> bulk(@RequestBody List<ArticleDTO> articleDTOs) {
         graphService.bulkArticles(articleDTOs); 
         return ResponseEntity.ok().body("OK");
     }
