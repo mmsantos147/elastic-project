eef35f6b551542d3e7a1e87c2fdd94fbb459d253
joaoragazzo <joao_ragazzo@hotmail.com>
2025-05-18 11:36:10 -0300

    feat: add responsiveness to mobile

--- frontend/src/components/results/IndexMenu/IndexResultMenu.jsx
+++ frontend/src/components/results/IndexMenu/IndexResultMenu.jsx
@@ -42,16 +42,16 @@ export const IndexResultMenu = () => {
   const { setIsIndexMenuOpen, indexMenuContent } = useSearchData();
   const { favoriteItem } = useFavoriteSearch();
   const { isLogged, verifyIfInFavorites } = useAuthData();
-  const [ modalOpen, setModalOpen ] = useState(false);
-  const [ isAlreadyFavorited, setIsAlreadyFavorited ] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [isAlreadyFavorited, setIsAlreadyFavorited] = useState(false);
+  const isMobile = window.innerWidth <= 768;
 
   useEffect(() => {
-
     if (verifyIfInFavorites(indexMenuContent.elasticId)) {
       setIsAlreadyFavorited(true);
     } else {
       setIsAlreadyFavorited(false);
-    };
+    }
   }, [indexMenuContent.elasticId]);
 
   return (
@@ -92,7 +92,9 @@ export const IndexResultMenu = () => {
             borderRadius: "999px",
             boxShadow: "none",
           }}
-          onClick={() => {window.location.href=indexMenuContent.url}}
+          onClick={() => {
+            window.location.href = indexMenuContent.url;
+          }}
         >
           <strong>{t("visit_the_site")}</strong> <RightOutlined />
         </Button>
@@ -103,18 +105,36 @@ export const IndexResultMenu = () => {
       </IndexResume>
 
       <Row style={{ display: "flex", justifyContent: "space-between" }}>
-        <OptionButton onClick={() => {setModalOpen(true)}}>
-          <ShareAltOutlined/>{t("share")}
+        <OptionButton
+          onClick={() => {
+            setModalOpen(true);
+          }}
+        >
+          <ShareAltOutlined />
+          {t("share")}
         </OptionButton>
-        <OptionButton onClick={() => {
-          isLogged ?  favoriteItem(indexMenuContent) : navigate("/login");}}>
-          <StarOutlined/>{t("save_result")}
+        <OptionButton
+          onClick={() => {
+            isLogged ? favoriteItem(indexMenuContent) : navigate("/login");
+          }}
+        >
+          <StarOutlined />
+          {t("save_result")}
         </OptionButton>
-        <OptionButton>{t("report_result")}</OptionButton>
-        <OptionButton>{t("Feedback")}</OptionButton>
+
+        {!isMobile && (
+          <>
+            <OptionButton>{t("report_result")}</OptionButton>
+            <OptionButton>{t("Feedback")}</OptionButton>
+          </>
+        )}
       </Row>
 
-      <ShareModal open={modalOpen} setOpen={setModalOpen} url={indexMenuContent.url} />
+      <ShareModal
+        open={modalOpen}
+        setOpen={setModalOpen}
+        url={indexMenuContent.url}
+      />
     </>
   );
 };
